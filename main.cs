6a00ad22938e609428b73d48d8d78b7dcdc9686f
TJ Schwartz <tj.schwartz2000@gmail.com>
2021-09-27 23:04:57 -0400

    Update main

---
+++ main.cs
@@ -0,0 +1,16 @@
+
+//Transpiler workie here
+/*
+ * Steps:
+ * Parse. Verify grammar
+ * Verify syntax is correct. No mistaken spaces, mistypes. No mispells.
+ * All brackets/braaces must close. Verify this.
+ * Parse tokens. Separate every single piece of code into tokens:
+ * identifiers, keywords, separators, operators, literal, comment
+ * 
+ * 
+
+int main()
+{
+
+}
---
+++ token.cs
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+public class token
+{
+
+    T type;
+    T value;
+    template <typename T>;
+
+
+    public token(T type, T value)
+    {
+
+    }
+}
+
+enum Tokens
+{
+    KEYW, OP, LTR, COM, SEP, ID
+};
